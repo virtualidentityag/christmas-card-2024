6f13e2fcddd5d64238d8f3f9fb0f5ed79de96aff
Marc Emmanuel <marc.emmauel@virtual-identity.com>
2024-11-22 12:37:38 +0100

    chore: remove drizzle

--- drizzle/0000_curvy_kabuki.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `score` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`username` text,
-	`score` integer
-);
---> statement-breakpoint
-CREATE UNIQUE INDEX `score_username_unique` ON `score` (`username`);
--- src/lib/server/db/schema/index.ts
+++
@@ -1,7 +0,0 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
-
-export const user = sqliteTable('score', {
-  id: integer('id').primaryKey(),
-  username: text('username').unique(),
-  score: integer('score'),
-});
