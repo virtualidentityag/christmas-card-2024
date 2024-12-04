72df5343ca2d9b6861791e91e8b397fcbc3ec663
Marc Emmanuel <marc.emmauel@virtual-identity.com>
2024-12-04 15:54:29 +0100

    implement mobile styles for home + tutorial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <div class="h-screen max-h-screen flex flex-col justify-center items-center text-center">
-	<p class="text-center tracking-wider max-w-screen-sm">
+	<p class="text-center tracking-wider max-w-screen-sm mb-12 md:mb-0">
 		<span class="text-3xl">Virtual Identity</span> <br /> <span class="text-xs">presents</span>
 	</p>
 
@@ -20,18 +20,18 @@
 		<img src={Logo} alt="" class="w-full" />
 	</div>
 
-	<p class="max-w-screen-sm text-center text-sm leading-6 font-light mb-4">
+	<p class="max-w-screen-sm text-center text-sm leading-6 font-light mb-4 mt-12">
 		The countdown is on! Virtual Identity is donating 9,000 € to charity this Christmas. You can
 		help to unlock up to 1,000 € on top. Catch as many decorations as you can to deck out your
 		Christmas tree before time runs out. Collect over 100 ornaments, and VI will add an extra €1 to
 		our charity donation.
 	</p>
 
-	<p class="max-w-screen-sm text-center text-sm leading-6 font-bold mb-12">
+	<p class="max-w-screen-sm text-center text-sm leading-6 font-bold mb-6 md:mb-12">
 		Ready, set... let the Deco Dash begin!
 	</p>
 
-	<div class="flex items-center justify-center gap-4 mb-12">
+	<div class="flex items-center justify-center gap-4 mb-6 md:mb-12 flex-col md:flex-row">
 		<Button href="/tutorial">Play now</Button>
 		<Button click={() => (showModal = true)} variant="secondary">Leaderboard</Button>
 	</div>
--- src/routes/tutorial/+page.svelte
+++ src/routes/tutorial/+page.svelte
@@ -6,42 +6,46 @@
 	import Bells from '$assets/images/bells.png';
 </script>
 
-<div class="h-screen max-h-screen py-16 px-24">
-	<div class="h-full flex flex-col pt-16">
-		<h1 class="text-5xl">How to play</h1>
+<div class="h-screen max-h-screen max-w-screen-lg py-16">
+	<div class="h-full flex flex-col pt-6 md:pt-16">
+		<h1 class="text-2xl md:text-5xl">How to play</h1>
 
-		<div class="py-14 border-b border-b-[#1C2E4F] grid grid-cols-2 items-center">
-			<p class="text-lg leading-relaxed">
+		<div
+			class="py-6 md:py-14 border-b border-b-[#1C2E4F] grid grid-cols-1 md:grid-cols-2 place-items-center"
+		>
+			<p class="md:text-lg leading-relaxed">
 				Catch decorations by moving your sock left or right using the arrow buttons on your
 				keyboard.
 			</p>
-			<img src={Keys} alt="" class="ms-12" />
+			<img src={Keys} alt="" class="mt-6 w-1/2" />
 		</div>
 
-		<div class="py-14 border-b border-b-[#1C2E4F] grid grid-cols-2 items-center">
-			<p class="text-lg leading-relaxed">
+		<div
+			class="pb-6 md:py-14 border-b border-b-[#1C2E4F] grid grid-cols-1 md:grid-cols-2 items-center"
+		>
+			<p class="md:text-lg leading-relaxed pt-6">
 				Collect or avoid special decorations to change the game.
 			</p>
 
-			<div class="grid grid-cols-3">
+			<div class="grid grid-cols-3 pt-6">
 				<figure class="flex flex-col items-center text-center">
-					<img src={Bells} alt="" class="w-20 h-20 mb-5" />
+					<img src={Bells} alt="" class="w-12 md:w-20 aspect-square mb-5" />
 					<figcaption>+5 seconds</figcaption>
 				</figure>
 
 				<figure class="flex flex-col items-center text-center">
-					<img src={Candy} alt="" class="w-20 h-20 mb-5" />
+					<img src={Candy} alt="" class="w-12 md:w-20 aspect-square mb-5" />
 					<figcaption>Caution: Smaller sock for 10 seconds</figcaption>
 				</figure>
 
 				<figure class="flex flex-col items-center text-center">
-					<img src={Star} alt="" class="w-20 h-20 mb-5" />
+					<img src={Star} alt="" class="w-12 md:w-20 aspect-square mb-5" />
 					<figcaption>Double points for 10 seconds</figcaption>
 				</figure>
 			</div>
 		</div>
 
-		<div class="flex-1 flex justify-end items-end">
+		<div class="flex-1 flex justify-end items-end pt-6">
 			<Button href="/game">Got it. Let's go!</Button>
 		</div>
 	</div>
