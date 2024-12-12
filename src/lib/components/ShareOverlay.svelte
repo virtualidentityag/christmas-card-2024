8b33fda862d241decf16decd7245329d2679f631
Marc Emmanuel <marc.emmauel@virtual-identity.com>
2024-12-12 13:35:18 +0100

    fix:share overlay styles

--- src/lib/components/ShareOverlay.svelte
+++ src/lib/components/ShareOverlay.svelte
@@ -19,8 +19,8 @@
 
 <Modal bind:show>
 	<h2 class="mb-6 text-4xl">Share this christmas experience with a colleague</h2>
-	<div class="flex">
-		<Input class="mr-6" value="https://christmas.virtual-identity.com" bind:element={input} />
+	<div class="flex flex-col">
+		<Input class="mr-6 mb-6" value="https://christmas.virtual-identity.com" bind:element={input} />
 		<Button class="" click={copy}>Copy</Button>
 	</div>
 </Modal>
